592c8c38a8d414ad6bdefe3012eb4bba742f6508
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-05-20 07:05:35 -0230

    login page init and signup page wip init

---
+++ src/login/Signup.tsx
@@ -0,0 +1,20 @@
+import { Signup1 } from "@/components/signup1";
+import uclball from "../assets/uclball.png";
+
+function Signup() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Signup1
+          logo={{
+            url: "/",
+            src: uclball,
+            alt: "Logo",
+            title: "FIFA Tracker"
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+export default Signup;
