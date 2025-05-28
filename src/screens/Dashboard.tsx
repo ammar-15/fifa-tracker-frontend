01d59e32b4a030c391548783916d1f4e67857576
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-05-28 09:23:44 -0230

    logout button very functional

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,12 +1,24 @@
 import SidebarLayout from "../components/SidebarLayout";
 import AddButton from "@/components/addbutton";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/");
+        }
+    }
+    , [navigate]);
   return (
     <SidebarLayout>
       <h1 className="text-4xl font-bold mb-4">Welcome to the Dashboard</h1>
       <p className="text-lg">This is your dashboard content.</p>
-      <div className="fixed bottom-10 right-12 z-50">
+      <div className="fixed bottom-10 right-12">
         <AddButton />
       </div>
     </SidebarLayout>
